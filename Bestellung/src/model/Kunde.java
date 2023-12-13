52c7a6270c71701fbd627b6fe1adf8417f4fa436
kschreiblehner <karsten.schreiblehner@gmail.com>
2023-12-13 11:46:10 +0100

    ADD setter und getter in Kunde.java

--- Bestellung/src/model/Kunde.java
+++ Bestellung/src/model/Kunde.java
@@ -1,23 +1,22 @@
 package model;
 
 public class Kunde {
-    protected int kundennummer;
-    String nachname;
-    String vorname;
-    String kundeSeit;
-    Bestellung[] bestellungen;
+    private int kundennummer;
+    private String nachname;
+    private String vorname;
+    private String kundeSeit;
+    private Bestellung[] bestellungen;
     // index der zuletzt hinzugefuegten Bestellung
-    int bestellungsIndex;
+    private int bestellungsIndex;
 
     public Kunde(int kundennummer, String nachname, String vorname, String kundeSeit) {
 
-        this.kundennummer = kundennummer;
-        this.nachname = nachname;
-        this.vorname = vorname;
-        this.kundeSeit = kundeSeit;
-        // kunde darf maximal 20 Bestellungen haben
-        this.bestellungen = new Bestellung[20];
-        this.bestellungsIndex = -1;
+        this.setKundennummer(kundennummer);
+        this.setNachname(nachname);
+        this.setVorname(vorname);
+        this.setKundeSeit(kundeSeit);
+        this.setBestellungen(new Bestellung[20]);
+        this.setBestellungsIndex(-1);
     }
 
     public boolean addBestellung(Bestellung bestellung) {
@@ -28,4 +27,56 @@ public class Kunde {
         return false;
     }
 
+    public int getKundennummer() {
+        return this.kundennummer;
+    }
+
+    public void setKundennummer(int kundennummer) {
+        this.kundennummer = kundennummer;
+    }
+
+    public String getNachname() {
+        return this.nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getVorname() {
+        return this.vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getKundeSeit() {
+        return this.kundeSeit;
+    }
+
+    public void setKundeSeit(String kundeSeit) {
+        this.kundeSeit = kundeSeit;
+    }
+
+    public Bestellung[] getBestellungen() {
+        return this.bestellungen;
+    }
+
+    public void setBestellungen(Bestellung[] bestellungen) {
+        // kunde darf maximal 20 Bestellungen haben
+        if (bestellungen.length <= 20) {
+            this.bestellungen = bestellungen;
+        } else {
+            System.err.println("Es können maximal 20 Bestellungen gespeichert werden.");
+        }
+    }
+
+    public int getBestellungsIndex() {
+        return this.bestellungsIndex;
+    }
+
+    public void setBestellungsIndex(int bestellungsIndex) {
+        this.bestellungsIndex = bestellungsIndex;
+    }
 }
